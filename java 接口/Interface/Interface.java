107998c35dbc1f8a9043dd4790badd46a676d2ed
SOYANGA <321830735@qq.com>
2018-10-22 22:59:37 +0800

    java 接口

---
+++ java 接口/Interface/Interface.java
@@ -0,0 +1,18 @@
+interface IMyInterFace {
+    // 全局常量
+    public static final String msg = "test";
+
+    // 抽象方法
+    public abstract void test();
+}
+
+class MyInterfaceImpl implements IMyInterFace {
+
+}
+
+class Interface {
+    public static void main(String[] args){
+        
+    } 
+    
+}
