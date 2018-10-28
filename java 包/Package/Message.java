0e193a0a599d0976bf2a5f1d764524eead9192de
SOYANGA <321830735@qq.com>
2018-10-28 23:34:59 +0800

    java 包

---
+++ java 包/Package/Message.java
@@ -0,0 +1,7 @@
+package www.bit.java.util;
+
+public class Message {
+    public void print() {
+        System.out.println("[Message] Hello Package");
+    }
+}
---
+++ java 包/Package/Test.java
@@ -0,0 +1,8 @@
+package www.bit.java;
+import www.bit.java.util.Message ;  // 导入程序类 
+public class Test {
+    public static void main(String[] args) {
+       Message msg =new Message();
+       msg.print();
+    }
+}
