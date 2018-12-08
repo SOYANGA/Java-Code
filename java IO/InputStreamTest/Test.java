adec474966be537f847175e713ec39fc710f1692
SOYANGA <321830735@qq.com>
2018-12-08 21:44:07 +0800

    java IO/InputStreamTest(字符输出流)

---
+++ java IO/InputStreamTest/Test.java
@@ -0,0 +1,22 @@
+package InputStreamTest;
+
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+public class Test {
+    public static void main(String[] args) throws Exception {
+//        File file = new File("C:" + File.separator + "Users" + File.separator
+//                + "32183" + File.separator + "Desktop" + File.separator + "TestHello.txt");
+//        InputStream in = new FileInputStream(file);
+//        byte[] data = new byte[1024];
+//        int len = in.read(data);
+//        String str = new String(data);
+//        System.out.println("问价" + str);
+//        in.close();
+        System.getProperties().list()
+
+    }
+}
